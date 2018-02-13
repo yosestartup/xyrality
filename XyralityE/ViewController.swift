09cda8bfc0361117412ecb2b364d8505806f2999
Oleksandr Bambuliak <yosestartup@gmail.com>
2018-02-13 12:20:59 +0200

    Initial commit

---
+++ XyralityE/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XyralityE
+//
+//  Created by Александр on 13.02.18.
+//  Copyright © 2018 hilton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
